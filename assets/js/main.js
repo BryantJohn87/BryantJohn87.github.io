0cc9462331b9692d31f0aeb5a4958cc7f8995929
leppid <myvoden@gmail.com>
2019-02-05 14:47:04 +0200

    desktop/tablet/mobile fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,27 +6,48 @@ $(document).ready(function() {
 
   $('.slider').slick({
     dots: true,
-    arrows: false,
+    arrows: true,
     autoplay: true,
     autoplaySpeed: 5000,
     centerPadding: '0px'
   });
 
-  $('.center').on('init', function(event, slick) {
+  $('.center').on('init', function(event) {
     $('.content').hide();
     $('.content[data-id=' + 1 + ']').show();
   });
-  $(".center").slick({
-    dots: false,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 3500,
-    infinite: true,
-    focusOnSelect: true,
-    centerMode: true,
-    slidesToShow: 3,
-    slidesToScroll: 3
-  });
+  if (screen.width > 500) {
+    $(".center").slick({
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 2500,
+      arrows: false,
+      speed: 700,
+      centerMode: true,
+      infinite: true,
+      centerPadding: '35%',
+      focusOnSelect: true,
+      pauseOnHover: false,
+      slidesToShow: 1,
+      slidesToScroll: 2
+    });
+  } else {
+    $(".center").slick({
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 2500,
+      arrows: false,
+      speed: 700,
+      centerMode: true,
+      infinite: true,
+      centerPadding: '33%',
+      pauseOnHover: false,
+      focusOnSelect: true,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    });
+  }
+
   $('.center').on('afterChange', function(event, slick, currentSlide, nextSlide) {
     $('.content').hide();
     $('.content[data-id=' + (currentSlide + 1) + ']').show();
@@ -47,7 +68,14 @@ $(document).ready(function() {
     // open the menu with slide effect
     $(".mobnav").hide();
   });
-  
+
+  $(document).mouseup(function (e){ // отслеживаем событие клика по веб-документу
+      var block = $(".mobnav"); // определяем элемент, к которому будем применять условия (можем указывать ID, класс либо любой другой идентификатор элемента)
+      if (!block.is(e.target) // проверка условия если клик был не по нашему блоку
+          && block.has(e.target).length === 0) { // проверка условия если клик не по его дочерним элементам
+          block.slideUp(300); // если условия выполняются - скрываем наш элемент
+      }
+  });
   var anchors = document.querySelectorAll('a[href*="#"]')
   for (let anchor of anchors) {
     anchor.addEventListener('click', function(e) {
@@ -65,18 +93,52 @@ $(document).ready(function() {
   $(".mcoldivs .coldiv2").hide()
   $(".mcoldivs .coldiv3").hide()
   $(".col1").click(function() {
+    $(".col2").removeClass("hovered")
+    $(".info2").removeClass("displayed")
+    $(".col3").removeClass("hovered")
+    $(".info3").removeClass("displayed")
     $(".mcoldivs .coldiv3").hide()
     $(".mcoldivs .coldiv2").hide()
     $(".mcoldivs .coldiv1").show()
   });
   $(".col2").click(function() {
     $(".mcoldivs .coldiv1").hide()
-    $(".mcoldivs .coldiv3").hide()
     $(".mcoldivs .coldiv2").show()
+    $(".mcoldivs .coldiv3").hide()
   });
   $(".col3").click(function() {
     $(".mcoldivs .coldiv1").hide()
     $(".mcoldivs .coldiv2").hide()
     $(".mcoldivs .coldiv3").show()
+  });
+
+  if ($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+    $(".col1").addClass("hovered")
+    $(".info1").slideDown(300)
+  };
+
+  $(".col1").hover(function() {
+    $(".col1").removeClass("hovered")
+    $(".info1").slideDown(300)
+    $(".info2").slideUp(10)
+    $(".info3").slideUp(10)
+  }, function() {
+    $(".info1").slideUp(300)
+  });
+  $(".col2").hover(function() {
+    $(".col1").removeClass("hovered")
+    $(".info1").slideUp(300)
+    $(".info2").slideDown(300)
+    $(".info3").slideUp(300)
+  }, function() {
+    $(".info2").slideUp(300)
+  });
+  $(".col3").hover(function() {
+    $(".col1").removeClass("hovered")
+    $(".info1").slideUp(300)
+    $(".info2").slideUp(300)
+    $(".info3").slideDown(300)
+  }, function() {
+    $(".info3").slideUp(300)
   });
 });
